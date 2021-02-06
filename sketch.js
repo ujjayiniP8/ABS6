9e9a81f289a5e1260816892dfb2805b47057827c
ujjayini08 <upoddar74@gmail.com>
2021-02-06 15:56:36 +0530

    added bird trajectory

--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
+var gameState= "onSling";
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,16 +71,21 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gameState!="launched")
+    {
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+  
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState="launched";
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+      //  slingshot.attach(bird.body);
     }
 }
